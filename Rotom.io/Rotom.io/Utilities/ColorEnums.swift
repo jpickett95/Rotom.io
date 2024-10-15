20c9b97b76c026c5638cc036a52fb23e0d0e2acd
Jonah Pickett <jpickettpre@gmail.com>
2024-10-14 21:11:19 -0400

    Added enums.

---
+++ Rotom.io/Rotom.io/Utilities/ColorEnums.swift
@@ -0,0 +1,104 @@
+//
+//  ColorEnums.swift
+//  Rotom.io
+//
+//  Created by Jonah Pickett on 10/14/24.
+//
+
+enum PokemonGameColor: String {
+    case national
+    case red = "game-red"
+    case blue = "game-blue"
+    case yellow = "game-yellow"
+    case gold = "game-gold"
+    case silver = "game-silver"
+    case Crystal = "game-crystal"
+    case ruby = "game-ruby"
+    case sapphire = "game-sapphire"
+    case emerald = "game-emerald"
+    case fireRed = "game-firered"
+    case leafGreen = "game-leafGreen"
+    case diamond = "game-diamond"
+    case pearl = "game-pearl"
+    case platinum = "game-platinum"
+    case soulSilver = "game-soulSilver"
+    case heartGold = "game-heartGold"
+    case black = "game-black"
+    case white = "game-white"
+    case black2 = "game-black2"
+    case white2 = "game-white2"
+    case x = "game-x"
+    case y = "game-y"
+    case omegaRuby = "game-omegaRuby"
+    case alphaSapphire = "game-alphaSapphire"
+    case sun = "game-sun"
+    case moon = "game-moon"
+    case ultraSun = "game-ultraSun"
+    case ultraMoon = "game-ultraMoon"
+    case letsGoPikachu = "game-letsGoPikachu"
+    case letsGoEvee = "game-letsGoEvee"
+    case sword = "game-sword"
+    case shield = "game-shield"
+    case brilliantDiamond = "game-brilliantDiamond"
+    case shiningPearl = "game-shiningPearl"
+    case legendsArceus = "game-legendsArceus"
+    case scarlet = "game-scarlet"
+    case violet = "game-violet"
+    case legendsZA = "game-legendsZA"
+}
+
+enum PokemonTypeColor: String {
+    case water = "type-water"
+    case stellar = "type-stellar"
+    case steel = "type-steel"
+    case rock = "type-rock"
+    case psychic = "type-psychic"
+    case poison = "type-poison"
+    case normal = "type-normal"
+    case ice = "type-ice"
+    case ground = "type-ground"
+    case grass = "type-grass"
+    case ghost = "type-ghost"
+    case flying = "type-flying"
+    case fire = "type-fire"
+    case fighting = "type-fighting"
+    case fairy = "type-fairy"
+    case electric = "type-electric"
+    case dragon = "type-dragon"
+    case dark = "type-dark"
+    case bug = "type-bug"
+    case unknown = "type-unknown"
+}
+
+enum PokemonEggGroupColor: String {
+    case notDiscovered = "eggGroup-notDiscovered"
+    case dragon = "eggGroup-dragon"
+    case ditto = "eggGroup-ditto"
+    case amorphous = "eggGroup-amorphous"
+    case water2 = "eggGroup-water2"
+    case mineral = "eggGroup-mineral"
+    case water3 = "eggGroup-water3"
+    case humanLike = "eggGroup-humanLike"
+    case grass = "eggGroup-grass"
+    case fairy = "eggGroup-fairy"
+    case field = "eggGroup-field"
+    case flying = "eggGroup-flying"
+    case bug = "eggGroup-bug"
+    case water1 = "eggGroup-water1"
+    case monster = "eggGroup-monster"
+}
+
+enum PokemonStatColor: String {
+    case attack = "stat-attack"
+    case defense = "stat-defense"
+    case hp = "stat-hp"
+    case specialAttack = "stat-specialAttack"
+    case specialDefense = "stat-specialDefense"
+    case speed = "stat-speed"
+}
+
+enum PokemonAttackTypeColor: String {
+    case physical = "attack-physical"
+    case special = "attack-special"
+    case status = "attack-status"
+}
---
+++ Rotom.io/Rotom.io/Utilities/Enums.swift
@@ -0,0 +1,85 @@
+//
+//  Enums.swift
+//  Rotom.io
+//
+//  Created by Jonah Pickett on 10/14/24.
+//
+
+enum Generation {
+    case national
+    case redBlueYellow
+    case goldSilver
+    case Crystal
+    case rubySapphire
+    case emerald
+    case fireRedLeafGreen
+    case diamondPearl
+    case platinum
+    case soulSilverHeartGold
+    case blackWhite
+    case black2White2
+    case xY
+    case omegaRubyAlphaSapphire
+    case sunMoon
+    case ultraSunMoon
+    case letsGo
+    case swordShield
+    case brilliantDiamondShiningPearl
+    case legendsArceus
+    case scarletViolet
+}
+
+enum PokemonType {
+    case water
+    case stellar
+    case steel
+    case rock
+    case psychic
+    case poison
+    case normal
+    case ice
+    case ground
+    case grass
+    case ghost
+    case flying
+    case fire
+    case fighting
+    case fairy
+    case electric
+    case dragon
+    case dark
+    case bug
+}
+
+enum PokemonEggGroup {
+    case notDiscovered
+    case dragon
+    case ditto
+    case amorphous
+    case water2
+    case mineral
+    case water3
+    case humanLike
+    case grass
+    case fairy
+    case field
+    case flying
+    case bug
+    case water1
+    case monster
+}
+
+enum PokemonStat {
+    case attack
+    case defense
+    case hp
+    case specialAttack
+    case specialDefense
+    case speed
+}
+
+enum PokemonAttackType {
+    case physical
+    case special
+    case status
+}
